7bbe97dee5ba8a294a335e9b7651c99b1bc062ec
michalkoziarski <michalkoziarski@gmail.com>
2017-11-16 13:06:19 +0100

    fixed dimension expansion for greyscale images

--- predict.py
+++ predict.py
@@ -51,7 +51,7 @@ def predict(images, session=None, network=None, targets=None):
         elif image.dtype == 'uint16':
             image = image / 65535.0
 
-        if len(image.shape) == 3:
+        if len(image.shape) == 2:
             image = np.expand_dims(rgb2gray(image), axis=2)
 
         prediction = network.outputs.eval(feed_dict={network.inputs: np.array([image])}, session=session)[0]
