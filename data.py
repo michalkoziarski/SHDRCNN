30cd1b90ee101865d42c47e8d4facf3433eb5174
michalkoziarski <michalkoziarski@gmail.com>
2018-05-28 13:46:19 +0200

    modified training data container: batch augmentation during sampling, extracting random patches from larger regions

--- data.py
+++ data.py
@@ -14,16 +14,16 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
 
 class TrainingSet:
     N_IMAGES = 74
-    N_AUGMENTATIONS = 8
 
-    def __init__(self, batch_size=64, patch_size=41, stride=21, n_channels=3, shape=(1000, 1500),
+    def __init__(self, batch_size=64, patch_size=41, region_size=100, stride=50, n_channels=3, shape=(1000, 1500),
                  discard_well_exposed=False):
         self.batch_size = batch_size
         self.patch_size = patch_size
+        self.region_size = region_size
         self.stride = stride
         self.n_channels = n_channels
         self.shape = shape
-        self.images_completed = 0
+        self.regions_completed = 0
         self.discard_well_exposed = discard_well_exposed
         self.epochs_completed = 0
         self.root_path = os.path.join(DATA_PATH, 'Training')
@@ -37,91 +37,99 @@ class TrainingSet:
 
         x_start = 0
 
-        while x_start + patch_size <= shape[0]:
+        while x_start + region_size <= shape[0]:
             y_start = 0
 
-            while y_start + patch_size <= shape[1]:
+            while y_start + region_size <= shape[1]:
                 self.length += 1
 
-                y_start += patch_size - stride
+                y_start += region_size - stride
 
-            x_start += patch_size - stride
+            x_start += region_size - stride
 
-        self.length *= self.N_IMAGES * self.N_AUGMENTATIONS
+        self.length *= self.N_IMAGES
 
-        self.images = np.empty((self.length, 2, self.patch_size, self.patch_size, self.n_channels), dtype=np.float32)
+        self.regions = np.empty((self.length, 2, region_size, region_size, n_channels), dtype=np.float32)
 
         current_image = 0
 
         for i in tqdm(range(1, self.N_IMAGES + 1)):
             image_directory = os.path.join(self.root_path, '%03d' % i)
 
-            ldr_image, hdr_image = _load_images(image_directory, self.n_channels, self.shape)
+            ldr_image, hdr_image = _load_images(image_directory, n_channels, shape)
 
             x_start = 0
 
-            while x_start + patch_size <= shape[0]:
-                x_end = x_start + patch_size
+            while x_start + region_size <= shape[0]:
+                x_end = x_start + region_size
                 y_start = 0
 
-                while y_start + patch_size <= shape[1]:
-                    y_end = y_start + patch_size
+                while y_start + region_size <= shape[1]:
+                    y_end = y_start + region_size
 
-                    ldr_patch = ldr_image[x_start:x_end, y_start:y_end].copy()
-                    hdr_patch = hdr_image[x_start:x_end, y_start:y_end].copy()
+                    ldr_region = ldr_image[x_start:x_end, y_start:y_end].copy()
+                    hdr_region = hdr_image[x_start:x_end, y_start:y_end].copy()
 
-                    if self.discard_well_exposed and not _under_or_over_exposed(ldr_patch):
-                        y_start += patch_size - stride
+                    if discard_well_exposed and not _under_or_over_exposed(ldr_region):
+                        y_start += region_size - stride
 
                         continue
 
-                    for _ in range(4):
-                        ldr_patch = np.rot90(ldr_patch)
-                        hdr_patch = np.rot90(hdr_patch)
+                    self.regions[current_image, 0] = ldr_region
+                    self.regions[current_image, 1] = hdr_region
 
-                        self.images[current_image, 0] = ldr_patch
-                        self.images[current_image, 1] = hdr_patch
+                    current_image += 1
 
-                        current_image += 1
+                    y_start += region_size - stride
 
-                    ldr_patch = np.fliplr(ldr_patch)
-                    hdr_patch = np.fliplr(hdr_patch)
+                x_start += region_size - stride
 
-                    for _ in range(4):
-                        ldr_patch = np.rot90(ldr_patch)
-                        hdr_patch = np.rot90(hdr_patch)
-
-                        self.images[current_image, 0] = ldr_patch
-                        self.images[current_image, 1] = hdr_patch
-
-                        current_image += 1
-
-                    y_start += patch_size - stride
-
-                x_start += patch_size - stride
-
-        if self.discard_well_exposed:
-            self.images = self.images[:current_image]
-            self.length = len(self.images)
+        if discard_well_exposed:
+            self.regions = self.regions[:current_image]
+            self.length = len(self.regions)
 
     def batch(self):
-        ldr_images = self.images[self.images_completed:(self.images_completed + self.batch_size), 0]
-        hdr_images = self.images[self.images_completed:(self.images_completed + self.batch_size), 1]
+        ldr_regions = self.regions[self.regions_completed:(self.regions_completed + self.batch_size), 0]
+        hdr_regions = self.regions[self.regions_completed:(self.regions_completed + self.batch_size), 1]
 
-        self.images_completed += self.batch_size
+        ldr_patches = []
+        hdr_patches = []
 
-        if self.images_completed >= self.length:
-            self.images_completed = 0
+        for i in range(len(ldr_regions)):
+            max_coordinate = self.region_size - self.patch_size
+
+            x = np.random.randint(max_coordinate)
+            y = np.random.randint(max_coordinate)
+
+            ldr_patch = ldr_regions[i, x:(x + self.patch_size), y:(y + self.patch_size)]
+            hdr_patch = hdr_regions[i, x:(x + self.patch_size), y:(y + self.patch_size)]
+
+            if np.random.choice([True, False]):
+                ldr_patch = np.fliplr(ldr_patch)
+                hdr_patch = np.fliplr(hdr_patch)
+
+            n_rotations = np.random.randint(4)
+
+            ldr_patch = np.rot90(ldr_patch, k=n_rotations)
+            hdr_patch = np.rot90(hdr_patch, k=n_rotations)
+
+            ldr_patches.append(ldr_patch)
+            hdr_patches.append(hdr_patch)
+
+        self.regions_completed += self.batch_size
+
+        if self.regions_completed >= self.length:
+            self.regions_completed = 0
             self.epochs_completed += 1
 
-        return ldr_images, hdr_images
+        return np.array(ldr_patches), np.array(hdr_patches)
 
     def shuffle(self):
         logging.info('Shuffling training partition...')
 
-        np.random.shuffle(self.images)
+        np.random.shuffle(self.regions)
 
-        self.images_completed = 0
+        self.regions_completed = 0
 
 
 class TestSet:
@@ -153,7 +161,7 @@ class TestSet:
 
             for i in tqdm(range(len(image_directories))):
                 image_directory = os.path.join(self.root_path, portion, image_directories[i])
-                ldr_image, hdr_image = _load_images(image_directory, self.n_channels, self.shape)
+                ldr_image, hdr_image = _load_images(image_directory, n_channels, shape)
 
                 self.ldr_images.append(ldr_image)
                 self.hdr_images.append(hdr_image)
